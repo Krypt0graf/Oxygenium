9674b548dbac428cbd13a956c94ff752274214af
M.Kirillov <M.Kirillov@treolan.ru>
2021-09-16 12:59:56 +0500

    comments & filepath from args

--- BrutBTC/Program.cs
+++ BrutBTC/Program.cs
@@ -14,22 +14,31 @@ namespace BrutBTC
     class Program
     {
         #region prop
-        public static double[] speeds;
-        public static Byte[] arr = new Byte[32];
+        public static double[] speeds; // Массив скоростей работы
+        public static Byte[] arr = new Byte[32];//Рандом массив
+        public static Random rnd = new Random();
         public static Dictionary<char, Dictionary<char, Dictionary<char, Dictionary<char, List<string>>>>> wallets 
-            = new Dictionary<char, Dictionary<char, Dictionary<char, Dictionary<char, List<string>>>>>();
+            = new Dictionary<char, Dictionary<char, Dictionary<char, Dictionary<char, List<string>>>>>(); // "Горячая" база кошельков, четырехуровневая
         #endregion
         static void Main(string[] args)
         {
-            
+            string file = string.Empty;
+            if (args.Length > 0)
+            {
+                file = args[0]; // Путь к фалу можно задать первым аргументом
+            }
+            else
+            {
+                Console.Write("Укажите относительный путь к файлу адресов: ");
+                file = Console.ReadLine(); // Иначе указываем
+            }
             Console.CursorVisible = false;
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Загрузка базы кошельков");
-            var timeload = loadbase("../list.txt");
+            var timeload = loadbase(file); // Грузим базу
             Console.Clear();
             Console.WriteLine($"База загружена за {timeload} сек");
-            Console.Write("Задайте количество потоков: ");
-            var count = Convert.ToInt32(Console.ReadLine());
+            var count = Environment.ProcessorCount; // Узнаем максимальное количество потоков
             Console.WriteLine();
 
             speeds = new double[count];
@@ -37,10 +46,10 @@ namespace BrutBTC
             for (int i = 0; i < count; i++)
             {
                 int j = i;
-                Thread thr = new Thread(() => brut(j));
-                thr.Priority = ThreadPriority.Highest;
+                Thread thr = new Thread(() => brut(j)); // Создаем поток, задаем ему индекс массива, куда он будет писать свою скорость работы
+                thr.Priority = ThreadPriority.Highest; // Выставялем максимальный приоритет
                 thr.Start();
-                //Task.Run(() => brut(j));
+                //Task.Run(() => brut(j)); // Можно стартануть как таск
                 Console.WriteLine($"Thread #{i}: 0 wallets/seconds");
             }
 
@@ -49,12 +58,12 @@ namespace BrutBTC
                 int end = 0;
                 for (int i = 0; i < count; i++)
                 {
-                    Console.SetCursorPosition(11, 3 + i);
-                    Console.WriteLine($"{ speeds[i] } wallets/seconds");
+                    Console.SetCursorPosition(11, 2 + i);
+                    Console.WriteLine($"{ speeds[i] } wallets/seconds"); // Обновляем инфу каждые 0,5 сек
                     end = i;
                 }
-                Console.SetCursorPosition(1, end + 5);
-                Console.WriteLine($"Total {speeds.Sum()}");
+                Console.SetCursorPosition(0, end + 4);
+                Console.WriteLine($"Total {speeds.Sum()}"); // Суммарная скорость
                 Thread.Sleep(500);
             }
         }
@@ -78,43 +87,48 @@ namespace BrutBTC
                     if (!wallets[str[1]][str[2]][str[3]].ContainsKey(str[4]))
                         wallets[str[1]][str[2]][str[3]].Add(str[4], new List<string>());
 
-                    wallets[str[1]][str[2]][str[3]][str[4]].Add(str);
+                    wallets[str[1]][str[2]][str[3]][str[4]].Add(str); // Указателями на вложеные словари являются первые 4 буквы ('1' пропускаем) адреса соответственно
                 }
                 timer.Stop();
-                return Math.Round((double)timer.ElapsedMilliseconds / 1000, 2);
+                return Math.Round((double)timer.ElapsedMilliseconds / 1000, 2); // Подсчитали время загрузки базы
             }
         }
         public static void brut(int taskIndex)
         {
-            Random rnd = new Random();
+            
             int i = 0;
             var t = Stopwatch.StartNew();
             while (true)
             {
-                rnd.NextBytes(arr);
-                var secret = getKey(arr);
-                var address = new BitcoinSecret(secret, Network.Main).GetAddress(ScriptPubKeyType.Legacy).ToString();
+                rnd.NextBytes(arr); // Заполняем рандомно массив
+                var secret = getKey(arr); // Получаем из него секрет
+                var address = new BitcoinSecret(secret, Network.Main).GetAddress(ScriptPubKeyType.Legacy).ToString(); // из секрета получаем адресс
                 try
                 {
-                    if (wallets[address[1]][address[2]][address[3]][address[4]].Any(w => w == address))
+                    if (wallets[address[1]][address[2]][address[3]][address[4]].Any(w => w == address)) // Проверяем
                     {
-                        using (StreamWriter w = new StreamWriter($"{address}.txt", true))
+                        using (StreamWriter w = new StreamWriter($"{address}.txt", true)) // Записываем в файл (имя файла - адрес, содержимое - секрет)
                         {
                             w.WriteLine(secret);
                         }
                     }
                 }
-                catch { }
-                if (i++ == 200)
+                catch { } // Если на каком то уровне отсутствует ключ для словаря
+                if (i++ == 200) // Ждем проверки нескольких адресов, для усреднения результата скорости
                 {
                     i = 0;
                     t.Stop();
                     var f = Math.Round(1000 / ((double)t.ElapsedMilliseconds / 200), 2);
-                    speeds[taskIndex] = f;
+                    speeds[taskIndex] = f; // Записываем скорость в свою ячейку
                     t = Stopwatch.StartNew();
                 };
             }
         }
+        /// <summary>
+        /// Получение секрета из массива байт
+        /// </summary>
+        /// <param name="bytes">Массив байт</param>
+        /// <returns></returns>
         static public string getKey(byte[] bytes)
         {
             var full = new byte[bytes.Length + 1];
